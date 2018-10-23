8a2e3ad5401b071f32bec11d93db54a1f011dc11
C* <flunchspam+github@gmail.com>
2018-10-23 11:55:52 +0200

    Update drag-and-drop.js

--- drag-and-drop.js
+++ drag-and-drop.js
@@ -30,27 +30,28 @@ DEALINGS IN THE SOFTWARE.
 //more or less stolen from a script in http://pastie.org/462639# by Paul Irish and an article from smashing magazine: http://coding.smashingmagazine.com/2010/05/23/make-your-own-bookmarklets-with-jquery/
 (function(){
 
-  var v = '1.8';
-
-  if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-    var done = false;
-    var script = document.createElement('script');
-    script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/' + v + '/jquery.min.js';
-    script.onload = script.onreadystatechange = function(){
-      if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
-        done = true;
-        loadDraggable();
+   $.extend($.expr[':'],{
+      absolute: function(el) {
+          return $(el).css('position') === 'absolute';
+      },
+      relative: function (el) {
+          return $(el).css('position') === 'relative';
+      },
+      static: function (el) {
+          return $(el).css('position') === 'static';
+      },
+      fixed: function (el) {
+          return $(el).css('position') === 'fixed';
       }
-    };
-    document.getElementsByTagName('head')[0].appendChild(script);
-  } else {
-    loadDraggable();
-  }
+  });
+  
+  var v = '1.8';  
+  
   
   function loadDraggable() {
     var done = false;
     var script = document.createElement('script');
-    script.src = 'http://code.jquery.com/ui/1.10.3/jquery-ui.js';
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js';
     script.onload = script.onreadystatechange = function(){
       if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
         done = true;
@@ -59,6 +60,8 @@ DEALINGS IN THE SOFTWARE.
     };
     document.getElementsByTagName('head')[0].appendChild(script);
   }
+  
+  loadDraggable();
 
   function draggThis(eve){
     var selectorString = $('#selectorInput').val();
@@ -68,7 +71,7 @@ DEALINGS IN THE SOFTWARE.
 
   function initMyBookmarklet() {
     (window.draggableBookmarklet = function() {
-      $('<div id=dndrQueryForm style="font-family:sans-serif;width:425px;padding:5px 10px;border-bottom-right-radius: 5px;border-bottom-left-radius: 5px;background:#f0f0f0;position:fixed;top:0px;left:50%;border:1px solid #999;border-top:none;-webkit-box-shadow:2px 2px 4px rgba(0, 0, 0, 0.3);-moz-box-shadow:2px 2px 4px rgba(0, 0, 0, 0.3);box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);z-index:990;"><label>selector: <strong>$(&apos;</strong><input type=text id=selectorInput><strong>&apos;).</strong></label> <button id=makeElementsDnDble style="float: right;">make Draggable!</button></div>').appendTo('body').on('click','#makeElementsDnDble',draggThis);
+      $('<div id=dndrQueryForm style="font-family:sans-serif;width:425px;padding:5px 10px;border-bottom-right-radius: 5px;border-bottom-left-radius: 5px;background:#f0f0f0;position:fixed;top:0px;left:50%;border:1px solid #999;border-top:none;-webkit-box-shadow:2px 2px 4px rgba(0, 0, 0, 0.3);-moz-box-shadow:2px 2px 4px rgba(0, 0, 0, 0.3);box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);z-index:990;"><label>selector: <strong>$(&apos;</strong><input type=text id=selectorInput value="*:absolute"><strong>&apos;).</strong></label> <button id=makeElementsDnDble style="float: right;">make Draggable!</button></div>').appendTo('body').on('click','#makeElementsDnDble',draggThis);
       $('#dndrQueryForm').draggable({ axis: 'x', addClasses: false });
       $('#selectorInput').keyup(function(eve){
         if(eve.which != 13)return;
